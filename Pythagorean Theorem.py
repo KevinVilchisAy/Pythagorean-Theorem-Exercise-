fa264147020e6831070297ecb71b2e2f8531087f
Kevin  Ayala Vilchis <166887015+KevinVilchisAy@users.noreply.github.com>
2024-10-05 17:23:39 -0600

    Add files via upload

---
+++ Pythagorean Theorem.py
@@ -0,0 +1,15 @@
+# user input for user's name
+userName = input('Enter your name: ')
+#will print userName
+print(userName)
+print()
+# user input for user's age (takesconverts the input to an int)
+age = int(input('Enter your Age: '))
+#prints user's age
+print(age)
+print()
+a = int(input('Enter number a:'))
+b = int(input('Enter number b:'))
+#formula of pythagorean theorem
+c = (a**2)+(b**2)
+print(c) 
